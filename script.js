60e99ce994bc8af04bfa9f1e12baf677f905e18b
Jangvelaaa <115176328+Jangvelaaa@users.noreply.github.com>
2022-11-29 16:22:39 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+//     1
+//
+//
+// let user = {
+//  firstname: "giorgi",
+//  lastname: "smith",
+//  age: 25,
+//  studentstatus: "active"
+//};
+//
+//
+//
+//console.log(user.studentstatus);
+///
+
+//     2
+//
+let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+if (Number < 89);
+
+//
+//onsole.log(numbers[0]);
+//onsole.log(numbers[1]);
+//onsole.log(numbers[2]);
+//onsole.log(numbers[3]);
+//onsole.log(numbers[4]);
+//onsole.log(numbers[5]);
+//onsole.log(numbers[6]);
+//onsole.log(numbers[7]);
+//onsole.log(numbers[8]);
+//onsole.log(numbers[9]);
+//onsole.log(numbers[10]);
+
+
+
+//
+//let user = {
+//  name: 'giorgi',
+//  age:  20,
+//  studentstatus: 'active'
+//}
+
+//let users = [//
+//  {username: 'giorgi', status: false},
+ // {username: 'levani', status: false},
+ // {username: 'anna', status: true}
+//]
+//
+//console.log(users.username.status=true);
+//
+//
+//
+//let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+// 
+//if numbers
